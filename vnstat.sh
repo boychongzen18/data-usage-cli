5482db49112706243eedf31a040e4136d4aad1b2
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2021-05-29 17:23:48 +0700

    Update vnstat.sh

--- vnstat.sh
+++ vnstat.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+echo " ################################### "
+
+echo "      Update & Installer Packet    "
+
+echo " ################################### "
 
 opkg update
 opkg install vnstat
+
+echo " ################################### "
+
+echo " Jangan Lupa Reboot STB Openwrt "
+
+echo "         Happy Preaking     "
+
+echo " ################################### "
